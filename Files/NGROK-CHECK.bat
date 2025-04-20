19f3475d6a1121099ba50555a3c50eb0f0f5ae27
Floreal2005 <48598777+Floreal2005@users.noreply.github.com>
2025-04-20 19:42:51 +0200

    Do not show IP on logs

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -5,8 +5,9 @@ REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" 
 net user administrator Adm1n1str4t0r! /add >nul
 net localgroup administrators administrator /add >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
-echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/agents"
+Rem echo IP:
+Rem tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/agents"
+echo IP: Open https://dashboard.ngrok.com/agents to find it
 echo User: Administrator
 echo Pass: Adm1n1str4t0r!
 curl -O https://raw.githubusercontent.com/Floreal2005/RDP/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
